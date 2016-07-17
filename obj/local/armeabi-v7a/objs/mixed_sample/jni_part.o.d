c3a6586e1391d757d1886c2e3fe262af569f5be5
ardipseptiadi <ardipseptiadi@gmail.com>
2016-07-17 21:22:45 +0700

    First Time Commit
    Commit All file from local repository

---
+++ obj/local/armeabi-v7a/objs/mixed_sample/jni_part.o.d
@@ -0,0 +1,133 @@
+obj/local/armeabi-v7a/objs/mixed_sample/jni_part.o: jni/jni_part.cpp \
+ C:/Users/ardipseptiadi/android/OpenCV-android-sdk/sdk/native/jni/include/opencv2/core/core.hpp \
+ C:/Users/ardipseptiadi/android/OpenCV-android-sdk/sdk/native/jni/include/opencv2/core.hpp \
+ C:/Users/ardipseptiadi/android/OpenCV-android-sdk/sdk/native/jni/include/opencv2/core/cvdef.h \
+ C:/Users/ardipseptiadi/android/OpenCV-android-sdk/sdk/native/jni/include/opencv2/core/hal/interface.h \
+ C:/Users/ardipseptiadi/android/NDK/build//../sources/cxx-stl/gnu-libstdc++/4.9/include/cstddef \
+ C:/Users/ardipseptiadi/android/OpenCV-android-sdk/sdk/native/jni/include/opencv2/core/version.hpp \
+ C:/Users/ardipseptiadi/android/OpenCV-android-sdk/sdk/native/jni/include/opencv2/core/base.hpp \
+ C:/Users/ardipseptiadi/android/NDK/build//../sources/cxx-stl/gnu-libstdc++/4.9/include/climits \
+ C:/Users/ardipseptiadi/android/NDK/build//../sources/cxx-stl/gnu-libstdc++/4.9/include/algorithm \
+ C:/Users/ardipseptiadi/android/OpenCV-android-sdk/sdk/native/jni/include/opencv2/core/cvstd.hpp \
+ C:/Users/ardipseptiadi/android/NDK/build//../sources/cxx-stl/gnu-libstdc++/4.9/include/cstring \
+ C:/Users/ardipseptiadi/android/NDK/build//../sources/cxx-stl/gnu-libstdc++/4.9/include/cctype \
+ C:/Users/ardipseptiadi/android/NDK/build//../sources/cxx-stl/gnu-libstdc++/4.9/include/string \
+ C:/Users/ardipseptiadi/android/NDK/build//../sources/cxx-stl/gnu-libstdc++/4.9/include/cmath \
+ C:/Users/ardipseptiadi/android/OpenCV-android-sdk/sdk/native/jni/include/opencv2/core/ptr.inl.hpp \
+ C:/Users/ardipseptiadi/android/OpenCV-android-sdk/sdk/native/jni/include/opencv2/core/neon_utils.hpp \
+ C:/Users/ardipseptiadi/android/OpenCV-android-sdk/sdk/native/jni/include/opencv2/core/traits.hpp \
+ C:/Users/ardipseptiadi/android/OpenCV-android-sdk/sdk/native/jni/include/opencv2/core/matx.hpp \
+ C:/Users/ardipseptiadi/android/OpenCV-android-sdk/sdk/native/jni/include/opencv2/core/saturate.hpp \
+ C:/Users/ardipseptiadi/android/OpenCV-android-sdk/sdk/native/jni/include/opencv2/core/fast_math.hpp \
+ C:/Users/ardipseptiadi/android/OpenCV-android-sdk/sdk/native/jni/include/opencv2/core/types.hpp \
+ C:/Users/ardipseptiadi/android/NDK/build//../sources/cxx-stl/gnu-libstdc++/4.9/include/cfloat \
+ C:/Users/ardipseptiadi/android/NDK/build//../sources/cxx-stl/gnu-libstdc++/4.9/include/vector \
+ C:/Users/ardipseptiadi/android/OpenCV-android-sdk/sdk/native/jni/include/opencv2/core/mat.hpp \
+ C:/Users/ardipseptiadi/android/OpenCV-android-sdk/sdk/native/jni/include/opencv2/core/bufferpool.hpp \
+ C:/Users/ardipseptiadi/android/OpenCV-android-sdk/sdk/native/jni/include/opencv2/core/mat.inl.hpp \
+ C:/Users/ardipseptiadi/android/OpenCV-android-sdk/sdk/native/jni/include/opencv2/core/persistence.hpp \
+ C:/Users/ardipseptiadi/android/OpenCV-android-sdk/sdk/native/jni/include/opencv2/core/operations.hpp \
+ C:/Users/ardipseptiadi/android/NDK/build//../sources/cxx-stl/gnu-libstdc++/4.9/include/cstdio \
+ C:/Users/ardipseptiadi/android/OpenCV-android-sdk/sdk/native/jni/include/opencv2/core/cvstd.inl.hpp \
+ C:/Users/ardipseptiadi/android/NDK/build//../sources/cxx-stl/gnu-libstdc++/4.9/include/complex \
+ C:/Users/ardipseptiadi/android/OpenCV-android-sdk/sdk/native/jni/include/opencv2/core/utility.hpp \
+ C:/Users/ardipseptiadi/android/OpenCV-android-sdk/sdk/native/jni/include/opencv2/core/core_c.h \
+ C:/Users/ardipseptiadi/android/OpenCV-android-sdk/sdk/native/jni/include/opencv2/core/types_c.h \
+ C:/Users/ardipseptiadi/android/OpenCV-android-sdk/sdk/native/jni/include/opencv2/core/optim.hpp \
+ C:/Users/ardipseptiadi/android/OpenCV-android-sdk/sdk/native/jni/include/opencv2/imgproc/imgproc.hpp \
+ C:/Users/ardipseptiadi/android/OpenCV-android-sdk/sdk/native/jni/include/opencv2/imgproc.hpp \
+ C:/Users/ardipseptiadi/android/OpenCV-android-sdk/sdk/native/jni/include/opencv2/imgproc/imgproc_c.h \
+ C:/Users/ardipseptiadi/android/OpenCV-android-sdk/sdk/native/jni/include/opencv2/imgproc/types_c.h \
+ C:/Users/ardipseptiadi/android/OpenCV-android-sdk/sdk/native/jni/include/opencv2/features2d/features2d.hpp \
+ C:/Users/ardipseptiadi/android/OpenCV-android-sdk/sdk/native/jni/include/opencv2/features2d.hpp \
+ C:/Users/ardipseptiadi/android/OpenCV-android-sdk/sdk/native/jni/include/opencv2/flann/miniflann.hpp \
+ C:/Users/ardipseptiadi/android/OpenCV-android-sdk/sdk/native/jni/include/opencv2/flann/defines.h \
+ C:/Users/ardipseptiadi/android/OpenCV-android-sdk/sdk/native/jni/include/opencv2/flann/config.h
+
+C:/Users/ardipseptiadi/android/OpenCV-android-sdk/sdk/native/jni/include/opencv2/core/core.hpp:
+
+C:/Users/ardipseptiadi/android/OpenCV-android-sdk/sdk/native/jni/include/opencv2/core.hpp:
+
+C:/Users/ardipseptiadi/android/OpenCV-android-sdk/sdk/native/jni/include/opencv2/core/cvdef.h:
+
+C:/Users/ardipseptiadi/android/OpenCV-android-sdk/sdk/native/jni/include/opencv2/core/hal/interface.h:
+
+C:/Users/ardipseptiadi/android/NDK/build//../sources/cxx-stl/gnu-libstdc++/4.9/include/cstddef:
+
+C:/Users/ardipseptiadi/android/OpenCV-android-sdk/sdk/native/jni/include/opencv2/core/version.hpp:
+
+C:/Users/ardipseptiadi/android/OpenCV-android-sdk/sdk/native/jni/include/opencv2/core/base.hpp:
+
+C:/Users/ardipseptiadi/android/NDK/build//../sources/cxx-stl/gnu-libstdc++/4.9/include/climits:
+
+C:/Users/ardipseptiadi/android/NDK/build//../sources/cxx-stl/gnu-libstdc++/4.9/include/algorithm:
+
+C:/Users/ardipseptiadi/android/OpenCV-android-sdk/sdk/native/jni/include/opencv2/core/cvstd.hpp:
+
+C:/Users/ardipseptiadi/android/NDK/build//../sources/cxx-stl/gnu-libstdc++/4.9/include/cstring:
+
+C:/Users/ardipseptiadi/android/NDK/build//../sources/cxx-stl/gnu-libstdc++/4.9/include/cctype:
+
+C:/Users/ardipseptiadi/android/NDK/build//../sources/cxx-stl/gnu-libstdc++/4.9/include/string:
+
+C:/Users/ardipseptiadi/android/NDK/build//../sources/cxx-stl/gnu-libstdc++/4.9/include/cmath:
+
+C:/Users/ardipseptiadi/android/OpenCV-android-sdk/sdk/native/jni/include/opencv2/core/ptr.inl.hpp:
+
+C:/Users/ardipseptiadi/android/OpenCV-android-sdk/sdk/native/jni/include/opencv2/core/neon_utils.hpp:
+
+C:/Users/ardipseptiadi/android/OpenCV-android-sdk/sdk/native/jni/include/opencv2/core/traits.hpp:
+
+C:/Users/ardipseptiadi/android/OpenCV-android-sdk/sdk/native/jni/include/opencv2/core/matx.hpp:
+
+C:/Users/ardipseptiadi/android/OpenCV-android-sdk/sdk/native/jni/include/opencv2/core/saturate.hpp:
+
+C:/Users/ardipseptiadi/android/OpenCV-android-sdk/sdk/native/jni/include/opencv2/core/fast_math.hpp:
+
+C:/Users/ardipseptiadi/android/OpenCV-android-sdk/sdk/native/jni/include/opencv2/core/types.hpp:
+
+C:/Users/ardipseptiadi/android/NDK/build//../sources/cxx-stl/gnu-libstdc++/4.9/include/cfloat:
+
+C:/Users/ardipseptiadi/android/NDK/build//../sources/cxx-stl/gnu-libstdc++/4.9/include/vector:
+
+C:/Users/ardipseptiadi/android/OpenCV-android-sdk/sdk/native/jni/include/opencv2/core/mat.hpp:
+
+C:/Users/ardipseptiadi/android/OpenCV-android-sdk/sdk/native/jni/include/opencv2/core/bufferpool.hpp:
+
+C:/Users/ardipseptiadi/android/OpenCV-android-sdk/sdk/native/jni/include/opencv2/core/mat.inl.hpp:
+
+C:/Users/ardipseptiadi/android/OpenCV-android-sdk/sdk/native/jni/include/opencv2/core/persistence.hpp:
+
+C:/Users/ardipseptiadi/android/OpenCV-android-sdk/sdk/native/jni/include/opencv2/core/operations.hpp:
+
+C:/Users/ardipseptiadi/android/NDK/build//../sources/cxx-stl/gnu-libstdc++/4.9/include/cstdio:
+
+C:/Users/ardipseptiadi/android/OpenCV-android-sdk/sdk/native/jni/include/opencv2/core/cvstd.inl.hpp:
+
+C:/Users/ardipseptiadi/android/NDK/build//../sources/cxx-stl/gnu-libstdc++/4.9/include/complex:
+
+C:/Users/ardipseptiadi/android/OpenCV-android-sdk/sdk/native/jni/include/opencv2/core/utility.hpp:
+
+C:/Users/ardipseptiadi/android/OpenCV-android-sdk/sdk/native/jni/include/opencv2/core/core_c.h:
+
+C:/Users/ardipseptiadi/android/OpenCV-android-sdk/sdk/native/jni/include/opencv2/core/types_c.h:
+
+C:/Users/ardipseptiadi/android/OpenCV-android-sdk/sdk/native/jni/include/opencv2/core/optim.hpp:
+
+C:/Users/ardipseptiadi/android/OpenCV-android-sdk/sdk/native/jni/include/opencv2/imgproc/imgproc.hpp:
+
+C:/Users/ardipseptiadi/android/OpenCV-android-sdk/sdk/native/jni/include/opencv2/imgproc.hpp:
+
+C:/Users/ardipseptiadi/android/OpenCV-android-sdk/sdk/native/jni/include/opencv2/imgproc/imgproc_c.h:
+
+C:/Users/ardipseptiadi/android/OpenCV-android-sdk/sdk/native/jni/include/opencv2/imgproc/types_c.h:
+
+C:/Users/ardipseptiadi/android/OpenCV-android-sdk/sdk/native/jni/include/opencv2/features2d/features2d.hpp:
+
+C:/Users/ardipseptiadi/android/OpenCV-android-sdk/sdk/native/jni/include/opencv2/features2d.hpp:
+
+C:/Users/ardipseptiadi/android/OpenCV-android-sdk/sdk/native/jni/include/opencv2/flann/miniflann.hpp:
+
+C:/Users/ardipseptiadi/android/OpenCV-android-sdk/sdk/native/jni/include/opencv2/flann/defines.h:
+
+C:/Users/ardipseptiadi/android/OpenCV-android-sdk/sdk/native/jni/include/opencv2/flann/config.h:
